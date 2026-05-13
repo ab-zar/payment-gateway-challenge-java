a97d90d49a0f359b83222815abb2b57b48ea505f
ab-zar <105588913+ab-zar@users.noreply.github.com>
2026-05-13 01:51:06 +0100

    add new models for bank payment

---
+++ src/main/java/com/checkout/payment/gateway/model/BankPaymentResponse.java
@@ -0,0 +1,25 @@
+package com.checkout.payment.gateway.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BankPaymentResponse {
+
+  private boolean authorized;
+
+  @JsonProperty("authorization_code")
+  private String authorizationCode;
+
+  public void setAuthorizationCode(String authorizationCode) {
+    this.authorizationCode = authorizationCode;
+  }
+
+  public boolean isAuthorized() {
+    return authorized;
+  }
+  public void setAuthorized(boolean authorized) {
+    this.authorized = authorized;
+  }
+  public String getAuthorizationCode() {
+    return authorizationCode;
+  }
+}
